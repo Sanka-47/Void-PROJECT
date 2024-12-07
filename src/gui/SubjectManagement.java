cbb026d8333a612302436f3b8bade365615ba0ff
Sanka-47 <kalindu47kk@gmail.com>
2024-12-07 11:59:25 +0530

    Update SubjectManagement.java

--- src/gui/SubjectManagement.java
+++ src/gui/SubjectManagement.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
- */
+//RANDKIA
 package gui;
 
 import java.sql.ResultSet;
