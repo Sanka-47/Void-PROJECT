3ffb060655f35bf1e37a2c1ebf5ddfb69eaa7206
KAVIGITHUB95 <kavishkajayawardana34@gmail.com>
2025-02-01 13:45:00 +0530

    Kavishka Commit 35

--- src/gui/StudentProgressandPerformanceTracking.java
+++ src/gui/StudentProgressandPerformanceTracking.java
@@ -28,7 +28,6 @@ public class StudentProgressandPerformanceTracking extends javax.swing.JPanel {
 
         jTable1.setDefaultRenderer(Object.class, renderer);
         jButton1.setEnabled(false);
-        jButton3.setEnabled(false);
     }
 
     private void loadassignmentID() {
@@ -77,7 +76,7 @@ public class StudentProgressandPerformanceTracking extends javax.swing.JPanel {
 //            e.printStackTrace();
 //        }
         try {
-            String sortOption = String.valueOf(jComboBox4.getSelectedItem());
+            String sortOption = String.valueOf(jComboBox2.getSelectedItem());
             String orderByClause = "";
 
             switch (sortOption) {
@@ -132,17 +131,17 @@ public class StudentProgressandPerformanceTracking extends javax.swing.JPanel {
 
     // Listener for real-time search
     private void setupSearchListener() {
-        jTextField4.getDocument().addDocumentListener(new DocumentListener() {
+        jTextField3.getDocument().addDocumentListener(new DocumentListener() {
             public void insertUpdate(DocumentEvent e) {
-                loadAssignmentDetails(jTextField4.getText().trim());
+                loadAssignmentDetails(jTextField3.getText().trim());
             }
 
             public void removeUpdate(DocumentEvent e) {
-                loadAssignmentDetails(jTextField4.getText().trim());
+                loadAssignmentDetails(jTextField3.getText().trim());
             }
 
             public void changedUpdate(DocumentEvent e) {
-                loadAssignmentDetails(jTextField4.getText().trim());
+                loadAssignmentDetails(jTextField3.getText().trim());
             }
         });
     }
@@ -173,7 +172,12 @@ public class StudentProgressandPerformanceTracking extends javax.swing.JPanel {
     private void reset() {
         jTextField1.setText("");
         jComboBox1.setSelectedItem("Select");
+        jTextField2.setText("");
+        jComboBox2.setSelectedItem("NIC ASC");
         jTextField3.setText("");
+        loadAssignmentDetails("");
+        jButton1.setEnabled(false);
+        jButton2.setEnabled(true);
     }
 
     @SuppressWarnings("unchecked")
@@ -184,7 +188,7 @@ public class StudentProgressandPerformanceTracking extends javax.swing.JPanel {
         jTextField1 = new javax.swing.JTextField();
         jLabel10 = new javax.swing.JLabel();
         jLabel12 = new javax.swing.JLabel();
-        jTextField3 = new javax.swing.JTextField();
+        jTextField2 = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
@@ -196,9 +200,9 @@ public class StudentProgressandPerformanceTracking extends javax.swing.JPanel {
         jLabel1 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        jComboBox4 = new javax.swing.JComboBox<>();
+        jComboBox2 = new javax.swing.JComboBox<>();
         jLabel5 = new javax.swing.JLabel();
-        jTextField4 = new javax.swing.JTextField();
+        jTextField3 = new javax.swing.JTextField();
         jButton4 = new javax.swing.JButton();
 
         jLabel7.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
@@ -215,7 +219,7 @@ public class StudentProgressandPerformanceTracking extends javax.swing.JPanel {
         jLabel12.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
         jLabel12.setText("Marks");
 
-        jTextField3.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
+        jTextField2.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
 
         jButton1.setFont(new java.awt.Font("Century Gothic", 1, 14)); // NOI18N
         jButton1.setText("Edit");
@@ -299,24 +303,24 @@ public class StudentProgressandPerformanceTracking extends javax.swing.JPanel {
         jLabel4.setFont(new java.awt.Font("Roboto", 1, 14)); // NOI18N
         jLabel4.setText("Sort By :");
 
-        jComboBox4.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "NIC ASC", "NIC DESC", "Marks ASC", "Marks DESC", "ID ASC", "ID DESC" }));
-        jComboBox4.addActionListener(new java.awt.event.ActionListener() {
+        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "NIC ASC", "NIC DESC", "Marks ASC", "Marks DESC", "ID ASC", "ID DESC" }));
+        jComboBox2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jComboBox4ActionPerformed(evt);
+                jComboBox2ActionPerformed(evt);
             }
         });
 
         jLabel5.setFont(new java.awt.Font("Century Gothic", 1, 14)); // NOI18N
         jLabel5.setText("Search");
 
-        jTextField4.addKeyListener(new java.awt.event.KeyAdapter() {
+        jTextField3.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyReleased(java.awt.event.KeyEvent evt) {
-                jTextField4KeyReleased(evt);
+                jTextField3KeyReleased(evt);
             }
         });
 
         jButton4.setFont(new java.awt.Font("Century Gothic", 1, 14)); // NOI18N
-        jButton4.setText("Clear All");
+        jButton4.setText("Clear Search and Sort");
         jButton4.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton4ActionPerformed(evt);
@@ -337,7 +341,7 @@ public class StudentProgressandPerformanceTracking extends javax.swing.JPanel {
                             .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 206, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 206, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel12)
-                            .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 206, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 206, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 206, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 206, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 206, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -350,14 +354,14 @@ public class StudentProgressandPerformanceTracking extends javax.swing.JPanel {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 60, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 13, Short.MAX_VALUE)
                         .addComponent(jLabel4)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jComboBox4, javax.swing.GroupLayout.PREFERRED_SIZE, 152, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 66, Short.MAX_VALUE)
+                        .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, 152, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 18, Short.MAX_VALUE)
                         .addComponent(jLabel5)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jButton4)
                         .addGap(18, 18, 18))))
@@ -373,10 +377,10 @@ public class StudentProgressandPerformanceTracking extends javax.swing.JPanel {
                         .addContainerGap()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel4)
-                            .addComponent(jComboBox4, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel5)
-                            .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                            .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel3)
                 .addGap(18, 18, 18)
@@ -392,7 +396,7 @@ public class StudentProgressandPerformanceTracking extends javax.swing.JPanel {
                         .addGap(22, 22, 22)
                         .addComponent(jLabel12)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -407,7 +411,7 @@ public class StudentProgressandPerformanceTracking extends javax.swing.JPanel {
                         .addGap(12, 12, 12))))
         );
 
-        jTextField3.getAccessibleContext().setAccessibleName("");
+        jTextField2.getAccessibleContext().setAccessibleName("");
         jComboBox1.getAccessibleContext().setAccessibleName("");
     }// </editor-fold>//GEN-END:initComponents
 
@@ -421,7 +425,7 @@ public class StudentProgressandPerformanceTracking extends javax.swing.JPanel {
             } else {
                 String nic = jTextField1.getText();
                 String assignmentId = String.valueOf(jComboBox1.getSelectedItem());
-                String marks = jTextField3.getText();
+                String marks = jTextField2.getText();
 
 //                if (nic.isEmpty()) {
 //                    JOptionPane.showMessageDialog(this, "Please enter NIC", "Warning", JOptionPane.WARNING_MESSAGE);
@@ -467,7 +471,7 @@ public class StudentProgressandPerformanceTracking extends javax.swing.JPanel {
         try {
             String nic = jTextField1.getText();
             String assignmentId = String.valueOf(jComboBox1.getSelectedItem());
-            String marks = jTextField3.getText();
+            String marks = jTextField2.getText();
 
 //            if (nic.isEmpty()) {
 //                JOptionPane.showMessageDialog(this, "Please enter NIC", "Warning", JOptionPane.WARNING_MESSAGE);
@@ -538,7 +542,7 @@ public class StudentProgressandPerformanceTracking extends javax.swing.JPanel {
 
             jTextField1.setText(nic);
             jComboBox1.setSelectedItem(assignmentId);
-            jTextField3.setText(marks);
+            jTextField2.setText(marks);
 
             jButton2.setEnabled(false);
             jButton1.setEnabled(true);
@@ -549,17 +553,18 @@ public class StudentProgressandPerformanceTracking extends javax.swing.JPanel {
         }
     }//GEN-LAST:event_jTable1MouseClicked
 
-    private void jComboBox4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox4ActionPerformed
-        loadAssignmentDetails(jTextField4.getText().trim());
-    }//GEN-LAST:event_jComboBox4ActionPerformed
+    private void jComboBox2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox2ActionPerformed
+        loadAssignmentDetails(jTextField3.getText().trim());
+    }//GEN-LAST:event_jComboBox2ActionPerformed
 
-    private void jTextField4KeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextField4KeyReleased
+    private void jTextField3KeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextField3KeyReleased
 
-    }//GEN-LAST:event_jTextField4KeyReleased
+    }//GEN-LAST:event_jTextField3KeyReleased
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        reset();
-        loadAssignmentDetails(""); // Reload assignments without sorting
+        jComboBox2.setSelectedItem("NIC ASC");
+        jTextField3.setText("");
+        loadAssignmentDetails("");
     }//GEN-LAST:event_jButton4ActionPerformed
 
 
@@ -569,7 +574,7 @@ public class StudentProgressandPerformanceTracking extends javax.swing.JPanel {
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
     private javax.swing.JComboBox<String> jComboBox1;
-    private javax.swing.JComboBox<String> jComboBox4;
+    private javax.swing.JComboBox<String> jComboBox2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel12;
@@ -582,7 +587,7 @@ public class StudentProgressandPerformanceTracking extends javax.swing.JPanel {
     private javax.swing.JTable jTable1;
     private javax.swing.JTable jTable2;
     private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
     // End of variables declaration//GEN-END:variables
 }
